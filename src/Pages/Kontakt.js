5fc277b28d7923419e2978635b97ab356a68b824
kaulkikass <kaulkikass@gmail.com>
2022-07-11 13:35:58 +0300

    Homework 8(1) - Add contacts show toggle for kontaktid and meist using useState, dynamic displaying

--- src/Pages/Kontakt.js
+++ src/Pages/Kontakt.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function Kontakt() {
+    const [n2itaTelKristiine, muudaN2itaTelKristiine] = useState(false);
+    const [n2itaTelYlemiste, muudaN2itaTelYlemiste] = useState(false);
+    const [n2itaTelTasku, muudaN2itaTelTasku] = useState(false);
+
     return ( <div>
         <div>See on kontaktide leht</div>
-        <div><img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="" /></div>
+        <div onClick={() => muudaN2itaTelKristiine(!n2itaTelKristiine)}>Kristiine keskus</div>
+        {n2itaTelKristiine && <div>32253424</div>}
+        <div onClick={() => muudaN2itaTelTasku(!n2itaTelTasku)}>Ülemiste keskus</div>
+        {n2itaTelTasku && <div>322534245</div>}
+        <div onClick={() => muudaN2itaTelYlemiste(!n2itaTelYlemiste)}>Tasku keskus</div>
+        {n2itaTelYlemiste && <div>322534247</div>}
     </div> );
 }
 
--- src/Pages/Meist.js
+++ src/Pages/Meist.js
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+
 function Meist() {
+    const [kontakt, n2itaKontakt] = useState('');
+
     return ( <div>
         <div>See on leht meist</div>
         <div>Siit leiad kõik vajaliku info meie kohta</div>
-        <div>Võta ühendust!</div>
+        <div>Meie töötajad:</div>
+        <br />
+        <div>Kaul</div>
+        <button onClick={() => n2itaKontakt('333333')}>Võta ühendust</button>
+        <div>Peeter</div>
+        <button onClick={() => n2itaKontakt('44444')}>Võta ühendust</button>
+        <div>Juula</div>
+        <button onClick={() => n2itaKontakt('55555555')}>Võta ühendust</button>
+        <br /><br />
+        { kontakt !== '' && <div>Tema kontakt: {kontakt}</div>}
+
     </div> );
 }
 
