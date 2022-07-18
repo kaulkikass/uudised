7e4480c209e4c7e19f3d2869086a0c3371345a5d
kaulkikass <kaulkikass@gmail.com>
2022-07-18 18:54:28 +0300

    Add Meist page contacts as objects and map cycle to show them

--- src/Pages/Meist.js
+++ src/Pages/Meist.js
@@ -2,19 +2,32 @@ import { useState } from 'react';
 
 function Meist() {
     const [kontakt, n2itaKontakt] = useState('');
+    const tootajad = [
+        {nimi: 'Kaul', telefon: '333333'},
+        {nimi: 'Peeter', telefon: '44444'},
+        {nimi: 'Juula', telefon: '55555555'}
+    ]
 
     return ( <div>
         <div>See on leht meist</div>
         <div>Siit leiad kõik vajaliku info meie kohta</div>
         <div>Meie töötajad:</div>
         <br />
-        <div>Kaul</div>
+        <div>{tootajad.map(tootaja =>
+            <div>
+                <div>{tootaja.nimi}</div>
+                <button onClick={() => n2itaKontakt(tootaja.telefon)}>Võta ühendust</button>
+                <br /><br />
+            </div>
+            )}
+        </div>
+{/*         <div>Kaul</div>
         <button onClick={() => n2itaKontakt('333333')}>Võta ühendust</button>
         <div>Peeter</div>
         <button onClick={() => n2itaKontakt('44444')}>Võta ühendust</button>
         <div>Juula</div>
         <button onClick={() => n2itaKontakt('55555555')}>Võta ühendust</button>
-        <br /><br />
+        <br /><br /> */}
         { kontakt !== '' && <div>Tema kontakt: {kontakt}</div>}
 
     </div> );
