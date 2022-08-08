18d21f622723c90bacd9e0f4fab5fb6d355608a0
kaulkikass <kaulkikass@gmail.com>
2022-08-08 16:05:03 +0300

    Add LisaUudis lehel uudise nime kontroll OnChange ja useRef()

--- src/Pages/LisaUudis.js
+++ src/Pages/LisaUudis.js
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function LisaUudis() {
     const uudiseRef = useRef();
+    const [s6num, uuendaS6num] = useState('');
 
     const lisaUusUudis = () => {
         let uudised = localStorage.getItem('uudised');
@@ -12,10 +13,23 @@ function LisaUudis() {
 
     }
 
+    const kontrolli = () => {
+        uuendaS6num("");
+        if (uudiseRef.current.value.charAt(0) === uudiseRef.current.value.toLowerCase()) {
+            uuendaS6num("Sisestasid uudise esimese tähe väikse tähena, palun paranda.");
+        } 
+            
+
+        if (uudiseRef.current.value.includes('  ')) {
+            uuendaS6num('Sisestasid 2 tühikut, palun paranda!')
+        } 
+    }
+
     return ( 
     <div>
+    <div>{s6num}</div>
         <label>Uudise nimi</label><br />
-        <input ref={uudiseRef} type="text" /> <br />
+        <input onChange={kontrolli} ref={uudiseRef} type="text" /> <br />
         <button onClick={() => lisaUusUudis()}>Lisa uudis</button>
     </div> 
     );
